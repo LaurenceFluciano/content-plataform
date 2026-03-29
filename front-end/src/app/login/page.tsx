b5bc3a3d0f3ba87503ce0811e15c01ecafeb6c84
laurence <laurencefluciano@gmail.com>
2026-03-29 15:07:37 -0300

    feat(routes): implement login page and layout

---
+++ front-end/src/app/login/page.tsx
@@ -0,0 +1,15 @@
+import BrandNavBar from "@/modules/components/molecule/BrandNavBar";
+import LoginForm from "@/modules/components/organism/LoginForm";
+
+export default function Page() {
+    return (
+        <>
+            <header>
+                <BrandNavBar />
+            </header>
+            <main className="w-full mt-5 flex items-center justify-center bg-surface-primary">
+                <LoginForm />
+            </main>
+        </>
+    );
+}
