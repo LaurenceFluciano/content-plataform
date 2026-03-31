075bcdd8d89ecfaf4a67f0ca68bbc2c0b34ff812
laurence <laurencefluciano@gmail.com>
2026-03-30 23:54:17 -0300

    refactor(group-digit-card): improve composition, state handling and navigation

--- front-end/src/modules/components/molecule/GroupDigitCard.tsx
+++ front-end/src/modules/components/molecule/GroupDigitCard.tsx
@@ -1,34 +1,67 @@
 'use client'
-import { useRef } from "react";
+import React, { useRef, useState } from "react";
 import DigitCard from "../atom/DigitCard";
-
+import { Digit } from "@/types/digit";
 
 interface GroupDigitCardProps {
     ammountDigitCards: number;
-    className: string;
+    className?: string;
 }
 
-export default function GroupDigitCard({ className, ammountDigitCards }: GroupDigitCardProps) {
+export default function GroupDigitCard({ ammountDigitCards, className }: GroupDigitCardProps) {
+    const [digits, setDigits] = useState<Digit[]>(Array(ammountDigitCards).fill(''));
     const digitCardRefs = useRef<(HTMLSpanElement | null)[]>([]);
+    const inputRef = useRef<HTMLInputElement | null>(null);
 
-    const handleNavigation = (index: number, signal: string) => {
-        if (signal.length === 1 && index < ammountDigitCards - 1) {
-            digitCardRefs.current[index + 1]?.focus();
-        } 
-        else if (signal === "backspace-empty" && index > 0) {
-            digitCardRefs.current[index - 1]?.focus();
-        }
-    };
+    const isValidDigit = (char: string) => {
+        return /^\d$/.test(char);
+    }
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        const fullValue = e.target.value; 
+
+        const newDigits = Array(ammountDigitCards).fill('') as Digit[];
+
+        fullValue.split('').forEach((char, i) => {
+            if (i < ammountDigitCards && isValidDigit(char)) {
+                newDigits[i] = char as Digit;
+            }
+        });
+
+        setDigits(newDigits);
+        
+    }
 
     return (
-        <div className="flex gap-2 justify-center">
-           {Array.from({ length: ammountDigitCards }).map((_, i) => (
-                <DigitCard className={className}
+        <>
+        
+        <input
+            ref={inputRef}
+            onChange={handleChange}
+            type="text"
+            value={digits.join('') || ''}
+            autoComplete="off"
+            maxLength={ammountDigitCards}
+            className="opacity-0 absolute"
+            name="otp-code"
+        />
+
+        <div className="flex gap-2 justify-center ">
+            {Array.from({ length: ammountDigitCards }).map((_, i) => (
+                <DigitCard
                     key={i}
-                    ref={(el) => { digitCardRefs.current[i] = el; }}
-                    onDigitChange={(val) => handleNavigation(i, val)}
+                    onClick={() => { 
+                        inputRef.current?.focus();
+                        
+                        inputRef.current?.setSelectionRange(i+1, i+1);
+                    }}
+                    ref={(el) => { digitCardRefs.current[i] = el; }} 
+                    className={className}
+                    digit={digits[i]}
                 />
             ))}
         </div>
+        </>
     );
 }
