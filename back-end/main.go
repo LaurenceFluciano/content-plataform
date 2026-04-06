e03aa174cdd6ad5a445baadec6b8268014cc40d5
laurence <laurencefluciano@gmail.com>
2026-04-06 16:55:42 -0300

    refactor(main): add first backend bootstrap

--- back-end/main.go
+++ back-end/main.go
@@ -1,24 +1,47 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/laurencefluciano/content-api/internal/config"
+	"github.com/laurencefluciano/content-api/internal/database"
 	"github.com/laurencefluciano/content-api/internal/user"
 )
 
 // "log"
 // "github.com/google/uuid"
-// "github.com/laurencefluciano/content-api/internal/config"
-// "github.com/laurencefluciano/content-api/internal/database"
 // "github.com/laurencefluciano/content-api/internal/user"
 
 func main() {
-	r := gin.Default()
+	/* --- Env Config --- */
+	config.InitEnv()
 
-	api := r.Group("/api/content-plataform/v1")
+	/* --- Database Init Config --- */
+	db, err := database.NewPostgresDB()
 
-	userHandler := &user.Handler{}
+	if err != nil {
+		log.Fatal("[ ERR ] Ocorreu um erro ao tentar conectar com o banco de dados: ", err)
+		return
+	}
 
-	userHandler.InitRoutes(api)
+	err = db.AutoMigrate(
+		user.GetModels(),
+	)
 
-	r.Run()
+	if err != nil {
+		log.Fatal("[ ERR ] Ocorreu um erro ao tentar conectar com o banco de dados: ", err)
+		return
+	}
+
+	user.NewRepository(db)
+
+	/* --- Routes Init Config --- */
+	routes := gin.Default()
+
+	api := routes.Group("/api/content-plataform/v1")
+
+	user.CreateHandler(api)
+
+	routes.Run()
 }
