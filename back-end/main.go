7bde8f7e60ef5ad423c3572f7292272592f09e48
laurence <laurencefluciano@gmail.com>
2026-04-05 22:38:23 -0300

    chore(main): perform routing smoke test

--- back-end/main.go
+++ back-end/main.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-	"log"
-
-	"github.com/google/uuid"
-	"github.com/laurencefluciano/content-api/internal/config"
-	"github.com/laurencefluciano/content-api/internal/database"
+	"github.com/gin-gonic/gin"
 	"github.com/laurencefluciano/content-api/internal/user"
 )
 
+// "log"
+// "github.com/google/uuid"
+// "github.com/laurencefluciano/content-api/internal/config"
+// "github.com/laurencefluciano/content-api/internal/database"
+// "github.com/laurencefluciano/content-api/internal/user"
+
 func main() {
-	config.InitEnv()
-	db, err := database.NewPostgresDB()
+	r := gin.Default()
 
-	if err != nil {
-		log.Fatal("Error ao efetuar a conexão com o banco de dados: ", err)
-	}
+	api := r.Group("/api/content-plataform/v1")
 
-	repo := user.NewRepository(db)
+	userHandler := &user.Handler{}
 
-	repo.CreateUser("Example", uuid.NewString(), user.Active)
+	userHandler.InitRoutes(api)
 
+	r.Run()
 }
