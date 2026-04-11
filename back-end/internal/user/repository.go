455a4ecde088132d2a020e3aa0a32a690ab36ecf
laurence <laurencefluciano@gmail.com>
2026-04-11 00:02:06 -0300

    feat(user): implement atomic user creation with role associations.

--- back-end/internal/user/repository.go
+++ back-end/internal/user/repository.go
@@ -1,6 +1,8 @@
 package user
 
 import (
+	"strings"
+
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
@@ -13,26 +15,32 @@ func NewRepository(db *gorm.DB) *Repository {
 	return &Repository{db: db}
 }
 
-/* --- CREATE USER --- */
-
 type SaveUserParams struct {
 	name   string
 	authId string
 	status Status
 }
 
+type UpdateUserProfile struct {
+	name      string
+	avatarUrl string
+	bio       string
+	websites  string
+	social    SocialLinks
+}
+
 func (r *Repository) CreateUser(params *SaveUserParams) (string, error) {
 	user := &User{
 		ID:     uuid.New(),
-		Name:   params.name,
+		Name:   strings.ToLower(params.name),
 		AuthId: params.authId,
 		Status: params.status,
 	}
 
-	result := r.db.Create(&user)
+	err := r.db.Create(&user).Error
 
-	if result.Error != nil {
-		return "", result.Error
+	if err != nil {
+		return "", err
 	}
 
 	return user.ID.String(), nil
@@ -48,4 +56,92 @@ func (r *Repository) GetUserById(id uuid.UUID) (error, *User) {
 	}
 
 	return nil, user
+}
+
+func (r *Repository) GetUserByAuthId(authId string) (*User, error) {
+	var user User
+
+	err := r.db.Where("auth_id = ?", authId).First(&user).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (r *Repository) GetProfileByAuthId(authId string) (*Profile, error) {
+	var u User
+
+	err := r.db.
+		Preload("Roles").
+		Preload("UserProfile").
+		Preload("ProducerProfile").
+		Where("auth_id = ?", authId).
+		First(&u).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Profile{
+		User:            &u,
+		UserProfile:     u.UserProfile,
+		ProducerProfile: u.ProducerProfile,
+	}, nil
+}
+
+func (r *Repository) UpdateProfileByAuthId(params *UpdateUserProfile, authId string) (string, error) {
+	var user User
+
+	err := r.db.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Preload("UserProfile").Preload("ProducerProfile").
+			Where("auth_id = ?", authId).First(&user).Error; err != nil {
+			return err
+		}
+
+		user.Name = strings.ToLower(params.name)
+
+		if err := tx.Save(&user).Error; err != nil {
+			return err
+		}
+
+		if user.UserProfile != nil {
+			user.UserProfile.AvatarUrl = params.avatarUrl
+			if err := tx.Save(user.UserProfile).Error; err != nil {
+				return err
+			}
+		}
+
+		if user.ProducerProfile != nil {
+			user.ProducerProfile.Bio = params.bio
+			user.ProducerProfile.Websites = params.websites
+			user.ProducerProfile.Social = params.social
+			if err := tx.Save(user.ProducerProfile).Error; err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	return user.ID.String(), nil
+}
+
+func (r *Repository) AddRole(userID uuid.UUID, roleID RoleType) error {
+	user := &User{
+		ID: userID,
+	}
+
+	err := r.db.Find(&user)
+
+	if err != nil {
+		return err.Error
+	}
+
+	return r.db.Model(user).Association("Roles").Append(&Role{RoleId: roleID})
 }
