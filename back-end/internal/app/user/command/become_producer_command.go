35439e7c010fce703e3da085b0b58c832364d5ad
LaurenceFluciano <laurencefluciano@gmail.com>
2026-04-20 22:44:20 -0300

    fix(user-usecase): separate command of a use case

---
+++ back-end/internal/app/user/command/become_producer_command.go
@@ -0,0 +1,5 @@
+package command
+
+type BecomeProducerCommand struct {
+	AuthId string
+}
--- back-end/internal/app/user/usecase/become_producer_use_case.go
+++ back-end/internal/app/user/usecase/become_producer_use_case.go
@@ -3,22 +3,24 @@ package usecase
 import (
 	"errors"
 
-	"github.com/laurencefluciano/content-api/internal/app/exception"
+	"github.com/laurencefluciano/content-api/internal/app/user/command"
 	"github.com/laurencefluciano/content-api/internal/domain/user"
+	"github.com/laurencefluciano/content-api/internal/exception"
 )
 
 type BecomeProducerUserCase struct {
 	Repo user.Repository
 }
 
-func (u *BecomeProducerUserCase) Execute(authId string) error {
-	entity, err := u.Repo.FindByAuthId(authId)
+func (u *BecomeProducerUserCase) Execute(cmd command.BecomeProducerCommand) error {
+	entity, err := u.Repo.FindByAuthId(cmd.AuthId)
 
 	if err != nil {
-		if errors.Is(err, exception.ErrNotFound) {
+		if errors.Is(err, exception.ErrRepoNotFound) {
 			return &exception.AppError{
 				Code:    exception.EntityNotFoundCode,
 				Message: "Esse usuário não existe.",
+				Err:     err,
 			}
 		}
 		return err
@@ -31,10 +33,11 @@ func (u *BecomeProducerUserCase) Execute(authId string) error {
 			return &exception.AppError{
 				Code:    exception.RoleAssignedCode,
 				Message: "Esse usuário já tem esse papel.",
+				Err:     err,
 			}
 		}
 		return err
 	}
 
-	return u.Repo.Save(entity)
+	return u.Repo.Update(entity)
 }
